c3132e59a6d1269123249c25098050cf5e41d3b2
ODonnchadha <danieldonahue1231@hotmail.com>
2020-08-29 06:36:52 -0500

    Parent-to-child communication.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  constructor() { }
+  values: any = {};
+
+  constructor(private http: HttpClient) { }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
-  ngOnInit() {
+  getValues() {
+    this.http.get('https://localhost:44362/api/values').subscribe(response => {
+      this.values = response;
+    }, error => {
+      console.log(error);
+    });
   }
 
+  ngOnInit() {
+    this.getValues();
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @Input() v: any;
   model: any = {};
 
   constructor() { }
@@ -16,5 +17,4 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
